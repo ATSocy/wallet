53f9acb3b5ac41b82c87f23516d12207ee77a65a
PlasmaKing <pk@atsocy.com>
2025-01-22 22:46:32 +0000

    fix: redact example password

--- utils/generate-pw-hash.py
+++ utils/generate-pw-hash.py
@@ -1,6 +1,6 @@
 import bcrypt
 
-password = "admin".encode()  # Password to hash
+password = "redacted".encode()  # Password to hash
 salt = bcrypt.gensalt(rounds=11)  # Generate a salt with a cost of 11
 hashed_password = bcrypt.hashpw(password, salt)  # Hash the password with the salt
 
